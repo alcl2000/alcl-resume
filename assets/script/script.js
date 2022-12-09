1f78b73b68809c14877ad1f30088a065fb709b37
alcl2000 <amy.555.lewis@gmail.com>
2022-12-09 18:03:02 +0000

    add: js project page styling component

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,28 @@
 const redCard = document.getElementById('card-3');
-
-window.onload = function() {
-    if (window.jQuery) {  
-        // jQuery is loaded  
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-};
+var orangeCard = document.getElementById('card-4');
+var yellowCard = document.getElementById('card-5');
 
 // Projects page
 $(redCard).mouseenter(function() {
     redCard.style.backgroundColor = '#FFEBEE';
 });
 $(redCard).mouseleave(function(){
-    redCard.style.backgroundColor = '#FFFFFF'
+    redCard.style.backgroundColor = '#FFFFFF';
+});
+
+$(orangeCard).mouseenter(function(){
+    this.style.backgroundColor = '#FFECB3';
+});
+$(orangeCard).mouseleave(function(){
+    this.style.backgroundColor = '#FFFFFF'
 })
+$(yellowCard).mouseenter(function (){
+    this.style.backgroundColor = '#FFF9C4'
+})
+$(yellowCard).mouseleave(function (){
+    this.style.backgroundColor = '#FFFFFF'
+})
+
 
 function fetchGitHubInformation(event){
     var username = $('#gh-username').val();
